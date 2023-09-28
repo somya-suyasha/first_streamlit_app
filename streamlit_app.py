cda7d67f84d057be84d04523b32ae068ef2a08ca
somya-suyasha <145769758+somya-suyasha@users.noreply.github.com>
2023-09-28 18:27:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,14 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
  
-
 # import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')  # setting fruit names in the picker
-
  
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Strawberries','Avocado'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
- 
 #Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
@@ -49,7 +46,7 @@ except URLError as e:
 streamlit.header("THE FRUIT LOAD LIST CONTAINS:")
 #snowflake related functions
 def get_fruit_load_list():
- with my_cnx.cursor() as my_curr:
+ with my_cnx.cursor() as my_cur:
   my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
   return my_cur.fetchall()
 
@@ -63,3 +60,4 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfru
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('FROM STREAMLIT')")
+
